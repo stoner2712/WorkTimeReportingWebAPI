4b23c85ab043c7843e0fa6800c989210abe9878b
Dariusz <dstorka@gmail.com>
2021-02-26 21:14:32 +0000

    Implement C*UD for Employee Entity

---
+++ DiplomaProject/DataTransferObjects/EmployeeDtos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomaProject.DataTransferObjects
+{
+    public class EmployeeDto
+    {
+        public long EmployeeId { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(60, ErrorMessage = "First name can't be longer than 60 characters")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string BuildingNumber { get; set; }
+        public string StreetName { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string JobTitle { get; set; }
+    }
+    public class EmployeeDtoCreate
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(60, ErrorMessage = "First name can't be longer than 60 characters")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string BuildingNumber { get; set; }
+        public string StreetName { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string JobTitle { get; set; }
+    }
+    public class EmployeeDtoUpdate
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(60, ErrorMessage = "First name can't be longer than 60 characters")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
